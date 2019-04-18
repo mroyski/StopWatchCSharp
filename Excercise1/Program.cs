e11a550e2fbf91cbafffc4e4c4e3aea2d942f648
Mike <mroyski@gmail.com>
2019-04-17 21:37:56 -0400

    initial commit

---
+++ Excercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new StopWatch();
+
+            Console.WriteLine("Enter 1 to start");
+            Console.WriteLine("Enter 2 to stop");
+            Console.WriteLine("Enter 3 to quit");
+            Console.WriteLine("****************");
+            bool GameTime = true;
+            while (GameTime)
+            {
+                var input = Console.ReadLine();
+                if (input == "1")
+                {
+                    watch.Start();
+                }
+                else if (input == "2")
+                {
+                    watch.Stop();
+                }
+                else if (input == "3")
+                {
+                    GameTime = false;
+                }
+            }
+        }
+    }
+}
---
+++ Excercise1/StopWatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excercise1
+{
+    public class StopWatch
+    {
+        private bool Running { get; set; }
+        private DateTime Started { get; set; }
+
+        public void Start()
+        {
+            if (this.Running)
+            {
+                Console.WriteLine("Already running...");
+                return;
+            }
+            else
+            {
+                this.Running = true;
+                this.Started = DateTime.Now;
+                Console.WriteLine(DateTime.Now);
+            }
+        }
+
+        public void Stop()
+        {
+            if (this.Running == false)
+            {
+                Console.WriteLine("Not running...");
+            }
+            else
+            {
+                this.Running = false;
+                var ended = DateTime.Now;
+                var totalTime = (ended - this.Started);
+                Console.WriteLine(totalTime);
+            }
+        }
+    }
+}
